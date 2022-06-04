1708910c520257c193c9a3c112b6d1ad392ae8e4
Hersh Nagpal <hershnagpal@gmail.com>
2022-06-04 05:25:16 -0400

    Icon done

---
+++ src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Icon } from './Icon';
+
+export default {
+    title: 'Design System/Atoms/Icon',
+    component: Icon,
+  } as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const PDFIcon = Template.bind({});
+PDFIcon.args = {
+  iconName: 'pdf',
+  text: 'Resume.pdf',
+};
+
+export const TextIcon = Template.bind({});
+TextIcon.args = {
+  iconName: 'text',
+  text: 'About.txt',
+};
+
+export const Link = Template.bind({});
+Link.args = {
+  iconName: 'link',
+  text: 'Resume.pdf',
+};
+
+export const FolderIcon = Template.bind({});
+FolderIcon.args = {
+  iconName: 'folder',
+  text: 'Folder',
+};
